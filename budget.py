9b998dbf15c37d33d7cfd244bd1516c7c4f70216
Emmanuel Mishiwo <emmachmish@gmail.com>
2026-01-20 07:55:16 +0000

    implemented the done command

--- budget.py
+++ budget.py
@@ -15,33 +15,42 @@ mod - to modify a budget amount using id
 
 
 '''
+fieldnames = ["Item", "Amount"]
+filename = "list.csv"
 
 
 @app.command()
 def add(item: str):
     Item = {}
-    Item["Item"] = input("Item: ")
+    Item["Item"] = item
     Item["Amount"] = input("Amount: ")
 
-    with open("list.csv", "a") as file:
-        fieldnames = ["Item", "Amount"]
+    with open(filename, "a", newline="") as file:
         writer = csv.DictWriter(file, fieldnames=fieldnames)
-        
         writer.writerow(Item)
     print("Item added succefully")
-    print(tabulate(Item, tablefmt="grid"))
-
-# def table():
-#     tab = []
-#     with open(list) as file:
-#         reader = csv.DictReader(file)
+    table()
 
 
-app.command()
+def table():
+    tab = []
+    with open(filename, "r") as file:
+        reader = csv.DictReader(file)
+        for line in reader:
+            tab.append(line)
+        print(tabulate(tab, tablefmt="grid"))
 
 
+@app.command()
 def view():
-    ...
+    table()
+
+
+@app.command()
+def done():
+    with open("list.csv", "w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fieldnames)
+        writer.writeheader()
 
 
 if __name__ == "__main__":
